042d0ff38ed5a2915bc1a3cc13980cd621a5bbfe
Konstantin Kiselev <Konstantin_2@mov.msu>
2012-07-12 16:22:20 +0400

    test

--- src/main/java/cc/redberry/core/tensor/iterator/TensorFirstIterator.java
+++ src/main/java/cc/redberry/core/tensor/iterator/TensorFirstIterator.java
@@ -1,8 +1,14 @@
 package cc.redberry.core.tensor.iterator;
 
+import cc.redberry.core.tensor.Tensor;
+
 /*
 *
 */
 public class TensorFirstIterator {
+     private final TreeTraverseIterator traverseIterator;
 
+         public TensorFirstIterator(Tensor tensor){
+             traverseIterator = new TreeTraverseIterator(tensor);
+         }
 }
