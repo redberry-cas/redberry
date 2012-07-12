3c7e93bc9f06742571dbf9b09fc8b0dbb9aa8fca
Stanislav Poslavsky <stvlpos@mail.ru>
2012-07-12 19:36:30 +0400

    Enhancement to tree iterators

--- src/main/java/cc/redberry/core/tensor/iterator/TensorFirstIterator.java
+++ src/main/java/cc/redberry/core/tensor/iterator/TensorFirstIterator.java
@@ -3,20 +3,17 @@ package cc.redberry.core.tensor.iterator;
 import cc.redberry.core.tensor.Tensor;
 
 /*
-* Wrapper for TreeTraverseIterator.
-* Return only <blockquote>Leaving</blockquote> elements.
-* Traverse into.
-*/
-public class TensorFirstIterator extends TensorIterator {
-    public TensorFirstIterator(Tensor tensor) {
-        super(tensor);
-    }
+ * Wrapper for TreeTraverseIterator. Return only
+ * <blockquote>Leaving</blockquote> elements. Traverse into.
+ */
+public final class TensorFirstIterator extends TreeIteratorAbstract {
 
     public TensorFirstIterator(Tensor tensor, TraverseGuide guide) {
-        super(tensor, guide);
+        super(tensor, guide, TraverseState.Entering);
     }
 
-    public Tensor next(){
-        return super.next(TraverseState.Leaving);
+    public TensorFirstIterator(Tensor tensor) {
+        super(tensor, TraverseState.Entering);
     }
+    
 }
--- src/main/java/cc/redberry/core/tensor/iterator/TensorLastIterator.java
+++ src/main/java/cc/redberry/core/tensor/iterator/TensorLastIterator.java
@@ -3,20 +3,16 @@ package cc.redberry.core.tensor.iterator;
 import cc.redberry.core.tensor.Tensor;
 
 /*
-* Wrapper for TreeTraverseIterator.
-* Return only <blockquote>Entering</blockquote> elements.
-* Traverse from out.
-*/
-public class TensorLastIterator extends TensorIterator {
-    public TensorLastIterator(Tensor tensor) {
-        super(tensor);
-    }
+ * Wrapper for TreeTraverseIterator. Return only
+ * <blockquote>Entering</blockquote> elements. Traverse from out.
+ */
+public final class TensorLastIterator extends TreeIteratorAbstract {
 
     public TensorLastIterator(Tensor tensor, TraverseGuide guide) {
-        super(tensor, guide);
+        super(tensor, guide, TraverseState.Leaving);
     }
 
-    public Tensor next(){
-        return super.next(TraverseState.Entering);
+    public TensorLastIterator(Tensor tensor) {
+        super(tensor, TraverseState.Leaving);
     }
 }
