41f166e65fd6a055e439a8f8447f72f9c9fb491e
Bolotin Dmitriy <bolotin.dmitriy@gmail.com>
2012-08-11 22:16:34 +0200

    Documentation editing started.

--- src/main/java/cc/redberry/core/tensor/Tensor.java
+++ src/main/java/cc/redberry/core/tensor/Tensor.java
@@ -18,21 +18,18 @@ package cc.redberry.core.tensor;
 import cc.redberry.core.context.Context;
 import cc.redberry.core.context.ToStringMode;
 import cc.redberry.core.indices.Indices;
+
 import java.util.Iterator;
 
 /**
- * Abstract class which postulates common tensor functionalities. This class
- * specifies common abstract methods, which inherent to every
- * <code>Tensor</code> implementation.
+ * <p>Abstract class which defines common tensor methods and properties.</p>
  * <p/>
- * <
- * p/> <h4><a name="Indices">Indices</a></h4> The most fundamental tensor
- * attribute is its indices. Interface
- * {@link cc.redberry.core.indices.Indices} provides common indices
- * functionalities. There are some
- * <code>Indices</code> implementations, and different implementations used in
- * different inheritors of
- * <code>Tensor</code>. Specified abstract method for getting Indices: <strong><code>Indices getIndices(),</code></strong>
+ * <h4>Indices</h4>
+ * <p>The most fundamental tensor attribute is its indices. Interface
+ * {@link cc.redberry.core.indices.Indices} provides common indices methods and properties.
+ * There are several <code>Indices</code> implementations, and different implementations used
+ * in different inheritors of <code>Tensor</code>. Specified abstract method for getting
+ * Indices: <strong><code>Indices getIndices(),</code></strong>
  * which returns indices of this
  * <code>Tensor</code>.
  * <p/>
@@ -105,7 +102,7 @@ import java.util.Iterator;
  */
 public abstract class Tensor
         implements Comparable<Tensor>,
-                   Iterable<Tensor> {
+        Iterable<Tensor> {
 
     /**
      * This method was make abstract to bind all inheritors have their hash code
@@ -116,7 +113,6 @@ public abstract class Tensor
      * in existing inheritors.
      *
      * @return hash code of this tensor
-     *
      * @see MultiTensor#hash()
      * @see SimpleTensor#hash()
      */
@@ -128,47 +124,69 @@ public abstract class Tensor
      * <code>Tensor</code>
      *
      * @return indices of this tensor
-     *
      * @see cc.redberry.core.indices.Indices
      */
     public abstract Indices getIndices();
 
     /**
-     * This method returns
-     * <code>TensorIterator</code> instance, which allows to iterate and set the
-     * tensor components of this during iterating. Such specification allows to
-     * separate tensor, transformation and comparison essences. Using
-     * <code>TensorIterator</code>, it is possible to go through tensor
-     * components of this, so we do not need to specify any code for
-     * transformations or comparisons algorithms in
-     * <code>Tensor</code> class.
+     * <p>Returns iterator over sub-tensors of current tensor (eg. summnads of sum, multipliers of
+     * product, etc..).</p>
+     * <p/>
+     * <p>For iteration through whole tensor tree use: {@link cc.redberry.core.tensor.iterator.TreeTraverseIterator}
+     * or implementations of {@link cc.redberry.core.tensor.iterator.TreeIteratorAbstract} abstract class.</p>
      *
-     * @return iterator
+     * @return iterator over subtensors
      */
     @Override
     public Iterator<Tensor> iterator() {
         return new BasicTensorIterator(this);
     }
 
+    /**
+     * <p>Returns i-th sub-tensor of this tensor (eg. summnad of sum, argument of tensor field etc...)</p>
+     *
+     * @param i index of sub-tensor
+     * @return i-th subtensor
+     */
     public abstract Tensor get(int i);
 
+    /**
+     * <p>Returns count of sub-tensors of this tensor.</p>
+     *
+     * @return count of sub-tensors
+     */
     public abstract int size();
 
-    public final Tensor set(int position, Tensor tensor) {
+    /**
+     * <p>Returns new tensor instance with i-th sub-tensor replaced by provided tensor.</p>
+     *
+     * @param i      index of sub-tensor to be replaced
+     * @param tensor tensor to replace i-th sub-tensor
+     * @return new instance of tensor
+     */
+    public final Tensor set(int i, Tensor tensor) {
         int size = size();
-        if (position >= size || position < 0)
+        if (i >= size || i < 0)
             throw new IndexOutOfBoundsException();
         if (tensor == null)
             throw new NullPointerException();
         TensorBuilder builder = getBuilder();
         for (int i = 0; i < size; ++i)
-            if (i == position)
+            if (i == i)
                 builder.put(tensor);
             else
                 builder.put(get(i));
         return builder.build();
     }
 
+    /**
+     * <p>Retrieves several sub-tensors from current tensor. This function is faster
+     * than sequential invocations of {@link #get(int)} method.</p>
+     *
+     * @param from index of first sub-tensor to be retrieved (inclusive)
+     * @param to   next index after last sub-tensor to be retrieved (exclusive)
+     * @return array with retrieved tensors
+     */
     public Tensor[] getRange(int from, final int to) {
         int size = size();
         if (from < 0 || from > to || to >= size)
@@ -180,56 +198,39 @@ public abstract class Tensor
     }
 
     /**
-     * Returns a string representation of tensor. Parameter
-     * {@link cc.redberry.core.context.ToStringMode} mode specifies
-     * representation mode. By default, we provides two print modes: <strong><code>ToStringMode.LaTeX</code></strong>
-     * and <strong><code>ToStringMode.UTF8</code></strong>. For example, in
-     * first case indices of tensor printing as their char representation, and
-     * in second as LaTeX code. Indices string representation specified in
-     * {@link cc.redberry.core.indices.Indices#toString(cc.redberry.core.context.ToStringMode)
-     * }.
-     * <p/>
-     * <p>Example code: <BR><code> Expression expr = Context.get().parser("A_mn/B");</code> <BR><code> System.out.println(expr.toString(ToStringMode.LaTeX));</code>
-     * <p>The result will be: <BR>
-     * <code>\frac{A_{m n}}{B}</code> <p><code> System.out.println(expr.toString(ToStringMode.UTF8))</code>
-     * <p>The result will be: <BR>
-     * <code>A_mn/B</code>
+     * <p>Returns a string representation of a tensor. See {@link cc.redberry.core.context.ToStringMode}
+     * for available modes.</p>
      *
-     * @param mode symbols printing mode (e.g.
-     * <code>UTF8</code> or
-     * <code>LaTeX</code>)
-     *
-     * @return a string representation of tensor
-     *
-     * @see cc.redberry.core.context.ToStringMode
-     * @see
-     * cc.redberry.core.indices.Indices#toString(cc.redberry.core.context.ToStringMode)
+     * @param mode printing mode (see. {@link cc.redberry.core.context.ToStringMode})
+     * @return a string representation of a tensor
      */
     public abstract String toString(final ToStringMode mode);
 
     /**
-     * @return {@code toString(CC.getDefaultPrintMode())}
+     * <p>Returns string representation of a tensor in default (see {@link cc.redberry.core.context.CC#getDefaultPrintMode()}) mode.
+     * <p>Equivalent to: <code>this.toString(CC.getDefaultPrintMode())</code>
+     * </p></p>
+     *
+     * @return string representation of a tensor in default mode
      */
     @Override
     public final String toString() {
         return toString(Context.get().getDefaultPrintMode());
     }
 
+    /**
+     * For internal use.
+     */
     protected String toString(ToStringMode mode, Class<? extends Tensor> clazz) {
         return toString(mode);
     }
 
     /**
-     * Comparing by hash code method.
-     *
-     * @param t tensor to compare
-     *
-     * @return < code>(hash() < t.hash() ? -1 : (hash() == t.hash() ? 0 : 1))</code>
+     * <p>Compares tensors by their hash code.</p>
      */
     @Override
     public final int compareTo(Tensor t) {
-        int hash = hash(), thash = t.hash();
-        return hash < thash ? -1 : (hash == thash ? 0 : 1);
+        return Integer.compare(hash(), t.hash());
     }
 
     @Override
