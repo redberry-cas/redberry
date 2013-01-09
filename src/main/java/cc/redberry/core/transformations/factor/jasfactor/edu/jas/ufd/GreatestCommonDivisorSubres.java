f65b17685e613723c6053811e601bbdad8295e51
Stanislav Poslavsky <stvlpos@mail.ru>
2013-01-09 14:24:06 +0400

    Copyright updated

--- src/main/java/cc/redberry/core/transformations/factor/jasfactor/edu/jas/ufd/GreatestCommonDivisorSubres.java
+++ src/main/java/cc/redberry/core/transformations/factor/jasfactor/edu/jas/ufd/GreatestCommonDivisorSubres.java
@@ -1,7 +1,7 @@
 /*
  * JAS: Java Algebra System.
  *
- * Copyright (c) 2000-2012:
+ * Copyright (c) 2000-2013:
  *    Heinz Kredel   <kredel@rz.uni-mannheim.de>
  *
  * This file is part of Java Algebra System (JAS).
